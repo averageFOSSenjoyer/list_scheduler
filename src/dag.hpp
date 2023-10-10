2c588f21cf8f88512457c47d87c6fc9752fc28cb
Frank Cai <frank.czr@gmail.com>
2023-10-09 21:46:42 -0500

    boost moment 2.0

--- src/dag.hpp
+++ src/dag.hpp
@@ -3,6 +3,7 @@
 
 #include <boost/unordered_map.hpp>
 #include <boost/unordered_set.hpp>
+#include <boost/container/set.hpp>
 #include <boost/tuple/tuple.hpp>
 #include <boost/tuple/tuple_comparison.hpp>
 #include <boost/graph/adjacency_list.hpp>
@@ -36,8 +37,8 @@ public:
         return ret;
     }
 
-    boost::unordered_set<boost::tuple<LABEL, LABEL>> getEdges() const {
-        boost::unordered_set<boost::tuple<LABEL, LABEL>> ret;
+    boost::container::set<boost::tuple<LABEL, LABEL>> getEdges() const {
+        boost::container::set<boost::tuple<LABEL, LABEL>> ret;
 
         for (const auto& v : g) {
             for (const auto& e : v.second) {
@@ -74,7 +75,7 @@ public:
 
     boost::unordered_set<LABEL> getStartingVertices() const {
         boost::unordered_set<LABEL> vertices = getVertices();
-        boost::unordered_set<boost::tuple<LABEL, LABEL>> edges = getEdges();
+        boost::container::set<boost::tuple<LABEL, LABEL>> edges = getEdges();
 
         for (const auto& e : edges) {
             if (vertices.find(e.template get<1>()) != vertices.end()) {
@@ -93,7 +94,7 @@ public:
             boost::add_vertex(v, boostGraph);
         }
 
-        boost::unordered_set<boost::tuple<LABEL, LABEL>> edges = getEdges();
+        boost::container::set<boost::tuple<LABEL, LABEL>> edges = getEdges();
         for (const auto& e : edges) {
             boost::add_edge(
                     boost::vertex_by_label(boost::get<0>(e), boostGraph),
